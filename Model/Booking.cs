3c6ae791317b74146202c26d925631e9709e15a4
FridaUltra <5carlsonfrida@gmail.com>
2025-01-09 14:23:51 +0100

    Add Nr of nights prop to the model

--- Model/Booking.cs
+++ Model/Booking.cs
@@ -17,6 +17,8 @@ public partial class Booking
 
     public DateOnly DepartureDate { get; set; }
 
+    public int NumberOfNights => (DepartureDate.ToDateTime(TimeOnly.MinValue) - ArrivalDate.ToDateTime(TimeOnly.MinValue)).Days;
+
     public int? TotalPrice { get; set; }
 
     public virtual Guest Guest { get; set; } = null!;
